5b572c21262e5edcbbce8c67b095a3201e9d0de4
haochengz <haochengzhao@outlook.com>
2018-10-01 17:16:47 +0800

    remove storybook temporariely and create tab navigation bar

--- App.js
+++ App.js
@@ -1,7 +1,98 @@
 
-import StorybookUI from './storybook';
-import App from './component/app';
+import React from 'react';
+import {
+  Text,
+  Button,
+  View
+} from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 
-// module.exports = __DEV__ ? StorybookUI : App;
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
 
-module.exports = App;
+class TrendingScreen extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class SearchScreen extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to details"
+          onPress={() => this.props.navigation.navigate('Details')}
+        />
+      </View>
+    );
+  }
+}
+
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
+
+const Stack = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Trending: TrendingScreen,
+    SearchScreen: SearchScreen,
+    Details: DetailsScreen
+  },
+  {
+    initialRouteName: 'Home'
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <Stack />;
+  }
+}
