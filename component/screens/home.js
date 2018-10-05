0852756b234572826c0c3fd49b3a3e14c750f708
haochengz <haochengzhao@outlook.com>
2018-10-05 22:50:42 +0800

    hot repositories list view prototype

--- component/screens/home.js
+++ component/screens/home.js
@@ -2,11 +2,12 @@
 import React from 'react';
 import {
   Text,
-  Button,
   View,
+  Image,
   StyleSheet,
   FlatList
 } from 'react-native';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
 
 export default class HomeScreen extends React.Component {
   constructor(props) {
@@ -21,29 +22,75 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            {name: 'Hoho', key: '1'},
-            {name: 'Bobo', key: '2'},
-            {name: 'Kiki', key: '3'},
-            {name: 'Wiwi', key: '4'},
-            {name: 'Cici', key: '5'}
-          ]}
-          renderItem={({item}) => (
-            <View>
-              <Text style={styles.item}>{ item.name }</Text>
-              <Text style={styles.item}
-              >
-                {this.state.data.name}
-              </Text>
-              <Button
-                key={item.name}
-                style={styles.btn}
-                title={`Go to ${item.name}'s detail page`}
-                onPress={() => this.props.navigation.navigate('Details')}
-              />
-            </View>
-          )}
+        <ScrollableTabView>
+          <RepoList tabLabel="react"></RepoList>
+          <RepoList tabLabel="node"></RepoList>
+          <RepoList tabLabel="koa"></RepoList>
+        </ScrollableTabView>
+      </View>
+    );
+  }
+}
+
+class RepoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  generateUrl(keyword) {
+    return `https://api.github.com/search/repositories?q=${keyword}&sort=stars&order=desc`;
+  }
+
+  async componentDidMount() {
+    const data = await this.loadData();
+    this.setState({
+      data: data.items
+    });
+  }
+
+  loadData() {
+    let url = this.generateUrl(this.props.tabLabel);
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(resolve)
+        .catch(error => reject(error));
+    });
+  }
+
+  render() {
+    return (
+      <FlatList
+        data={this.state.data}
+        keyExtractor={item => item.id}
+        renderItem={({item}) => <RepoCell repo={item} />}
+      />
+    );
+  }
+}
+
+class RepoCell extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>{this.props.repo.full_name}</Text>
+        <Text>{this.props.repo.description}</Text>
+        <Image
+          source={{uri: this.props.repo.owner.avatar_url}}
+          style={styles.avatar}
         />
       </View>
     );
@@ -64,5 +111,9 @@ const styles = StyleSheet.create({
   },
   btn: {
     fontSize: 14
+  },
+  avatar: {
+    width: 20,
+    height: 20
   }
 });
