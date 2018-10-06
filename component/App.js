75092f7ef4d2e416ba3fa25f9e45573b2fd6c393
haochengz <haochengzhao@outlook.com>
2018-10-06 18:45:09 +0800

    style of navigation bar

--- component/App.js
+++ component/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import {
   Text,
   Button,
+  Image,
   View
 } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 
 import HomeScreen from './screens/home';
-import BGC from './common/theme';
+import {BGC, FC, SFC} from './common/theme';
 
 class HomeScreenNav extends React.Component {
   render() {
@@ -80,15 +81,70 @@ class DetailsScreen extends React.Component {
 
 const HomeStack = createBottomTabNavigator(
   {
-    Home: HomeScreenNav,
-    Trending: TrendingScreen,
-    SearchScreen: SearchScreen,
-    Details: DetailsScreen
+    Home: {
+      screen: HomeScreenNav,
+      navigationOptions: screenNavigationOptions('HOME',
+        require('../asset/icon/iconmonstr-home-6-240.png'),
+        require('../asset/icon/iconmonstr-home-7-240.png'))
+    },
+    Trending: {
+      screen: TrendingScreen,
+      navigationOptions: screenNavigationOptions('TREND',
+        require('../asset/icon/iconmonstr-whats-hot-1-240.png'),
+        require('../asset/icon/iconmonstr-whats-hot-2-240.png'))
+    },
+    SearchScreen: {
+      screen: SearchScreen,
+      navigationOptions: screenNavigationOptions('FAVORITE',
+        require('../asset/icon/iconmonstr-star-3-240.png'),
+        require('../asset/icon/iconmonstr-star-5-240.png'))
+    },
+    Details: {
+      screen: DetailsScreen,
+      navigationOptions: screenNavigationOptions('DETAIL',
+        require('../asset/icon/iconmonstr-menu-5-240.png'),
+        require('../asset/icon/iconmonstr-menu-6-240.png'))
+    }
   },
   {
     initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: BGC,
+      inactiveTintColor: SFC,
+      activeBackgroundColor: FC,
+      inactiveBackgroundColor: FC
+    }
   }
 );
+
+function screenNavigationOptions(title, focused_icon, unfocused_icon) {
+  return function() {
+    return {
+      title: title,
+      tabBarIcon: ({focused, tintColor}) => (
+        focused ? <Image
+          source={focused_icon}
+          resizeMode="contain"
+          style={{
+            width: 23,
+            height: 23,
+            tintColor: tintColor
+          }}
+        />
+          :
+          <Image
+            source={unfocused_icon}
+            resizeMode="contain"
+            style={{
+              width: 23,
+              height: 23,
+              tintColor: tintColor
+            }}
+          />
+      )
+    };
+  };
+}
 
 export default class Home extends React.Component {
   render() {
