c52739a52b6ee9d54c3b73ef13fe21cca057c5d0
haochengz <haochengzhao@outlook.com>
2018-10-09 17:09:14 +0800

    feat: tags DAO using AsyncStorage

---
+++ component/common/dao/tag-dao.js
@@ -0,0 +1,51 @@
+
+import {
+  AsyncStorage
+} from 'react-native';
+
+export default class TagDao {
+  constructor() {
+    this.defaultSet = [
+      {title: 'languages', data: ['Java', 'Javascript', 'Python']},
+      {title: 'frameworks', data: ['Spring', 'Angular', 'Django']},
+      {title: 'tools', data: ['Atom', 'Git', 'Sublime']}
+    ];
+    this.setDefault = false;
+  }
+
+  async loadDefault() {
+    try {
+      const prestine = await AsyncStorage.getItem('@tag:prestine');
+      if(prestine === null) {
+        await AsyncStorage.setItem('@tag:set', JSON.stringify(this.defaultSet));
+        this.setDefault = true;
+      }
+    } catch(error) {
+      throw error;
+    }
+  }
+
+  getDefault() {
+    return this.defaultSet;
+  }
+
+  async getTags() {
+    if(false === this.setDefault) {
+      await this.loadDefault();
+    }
+    const result = await AsyncStorage.getItem('@tag:set');
+    return JSON.parse(result);
+  }
+
+  async setTags(tags) {
+    await AsyncStorage.setItem('@tag:set', tags);
+  }
+
+  // async addTag(tag, section){}
+
+  // async removeTag(tag, section) {}
+
+  // async addSection(section) {}
+
+  // async removeSection(section) {}
+}
--- component/screens/options/tags.js
+++ component/screens/options/tags.js
@@ -1,15 +1,79 @@
 
 import React from 'react';
 import {
-  Text, View
+  Text, View, SectionList, StyleSheet, Switch
 } from 'react-native';
 
+import TagDao from '../../common/dao/tag-dao';
+const tagDao = new TagDao();
+
 export default class TagSetupPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionLoad: false,
+      sections: tagDao.getDefault()
+    };
+  }
+
+  async componentDidMount() {
+    const sections = await tagDao.getTags();
+    this.setState({
+      sections: sections,
+      sectionLoad: true
+    });
+  }
+
   render() {
     return (
-      <View>
-        <Text>this is tag setup page</Text>
+      <View style={styles.container}>
+        <SectionList
+          sections={this.state.sections}
+          renderItem={({item}) => (
+            <View style={styles.itemContainer}>
+              <Text style={styles.itemText}>{item}</Text>
+              <View style={styles.itemSwitch}>
+                <Switch />
+              </View>
+            </View>
+          )}
+          renderSectionHeader={({section}) => <Text
+            style={styles.header}
+          >{section.title}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 22,
+    backgroundColor: 'white'
+  },
+  header: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: 'grey'
+  },
+  itemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  itemText: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  },
+  itemSwitch: {
+    flex: 1,
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end'
+  }
+});
