27882db356c84e6d59bd47aa7bb8192dcbd77992
haochengz <haochengzhao@outlook.com>
2018-10-09 23:54:49 +0800

    add tag sorting setup page

---
+++ component/screens/options/tag-sort.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+export default class TagSortPage extends React.Component {
+  render() {
+    return (
+      <Text>This is the test sort page</Text>
+    );
+  }
+}
